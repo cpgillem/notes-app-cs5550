2bf9751a6e1429815dc5f9db7f7d3c3e3429d37d
Cade Gillem <cpgillem@gmail.com>
2017-10-28 14:03:39 -0400

    Add other tables to database setup function.

--- db.go
+++ db.go
@@ -7,16 +7,54 @@ import (
 )
 
 // SetUpDB creates all the tables necessary for the app.
-func SetUpDB(db *sql.DB) {
-	db.Exec(`CREATE TABLE users (
-				id	INT(10) NOT NULL UNIQUE AUTO_INCREMENT,
+func SetUpDB(db *sql.DB) error {
+	_, err := db.Exec(`CREATE TABLE users (
+				id		INT(10) NOT NULL UNIQUE AUTO_INCREMENT,
 				name	VARCHAR(191) NOT NULL UNIQUE,
 				admin	BOOLEAN DEFAULT FALSE NOT NULL,
 				PRIMARY KEY (id)
 			)`)
+	if err != nil {
+		return err
+	}
+	
+	_, err = db.Exec(`CREATE TABLE notes (
+				id		INT(10) NOT NULL UNIQUE AUTO_INCREMENT,
+				title	VARCHAR(191) NOT NULL,
+				content TEXT,
+				time	DATETIME,
+				user_id	INT(10) NOT NULL,
+				PRIMARY KEY (id)
+			)`)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec(`CREATE TABLE tags (
+				id		INT(10) NOT NULL UNIQUE AUTO_INCREMENT,
+				title	VARCHAR(191) NOT NULL,
+				PRIMARY KEY (id)
+			)`)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec(`CREATE TABLE note_tag (
+				note_id	INT(10) NOT NULL,
+				tag_id	INT(10) NOT NULL,
+				PRIMARY KEY (note_id, tag_id)
+			)`)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // TearDownDB clears the database of all tables that the app uses.
 func TearDownDB(db *sql.DB) {
 	db.Exec("DROP TABLE IF EXISTS users")
+	db.Exec("DROP TABLE IF EXISTS notes")
+	db.Exec("DROP TABLE IF EXISTS tags")
+	db.Exec("DROP TABLE IF EXISTS note_tag")
 }
