fb3ca7a7ba30684721ca5181f52fd6b096035075
Cade Gillem <cpgillem@gmail.com>
2017-10-30 10:13:09 -0400

    Add many-to-many notes retrieval.

--- models_test.go
+++ models_test.go
@@ -90,3 +90,43 @@ func TestUserNotes(t *testing.T) {
 	AssertEqual("title1", notes[0].Title, t)
 	AssertEqual("title2", notes[1].Title, t)
 }
+
+func TestTagNotes(t *testing.T) {
+	db := SetUpDbTest()
+	defer TearDownDbTest(db)
+
+	// Prepare the database.
+	res, err := db.Exec("INSERT INTO tags (title) VALUES ('tag')")
+	tID, err := res.LastInsertId()
+	res, err = db.Exec("INSERT INTO users (name) VALUES ('test')")
+	uID, err := res.LastInsertId()
+	res, err = db.Exec("INSERT INTO notes (title, user_id) VALUES ('title1', ?)", uID)
+	n1ID, err := res.LastInsertId()
+	res, err = db.Exec("INSERT INTO notes (title, user_id) VALUES ('title2', ?)", uID)
+	n2ID, err := res.LastInsertId()
+	_, err = db.Exec("INSERT INTO note_tag (note_id, tag_id) VALUES (?, ?)", n1ID, tID)
+	_, err = db.Exec("INSERT INTO note_tag (note_id, tag_id) VALUES (?, ?)", n2ID, tID)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create mock model.
+	tag := Tag {
+		Resource: Resource {
+			ID: tID,
+			DB: db,
+			Table: "tags",
+		},
+		Title: "tag",
+	}
+
+	// Retrieve the tag's notes.
+	ns, err := tag.Notes()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	AssertEqual("title1", ns[0].Title, t)
+	AssertEqual("title2", ns[1].Title, t)
+}
