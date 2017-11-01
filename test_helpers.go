74634d03b9a3558d2be75e871c0f89d0c402c3d3
Cade Gillem <cpgillem@gmail.com>
2017-10-31 21:30:11 -0400

    Add a mocking test helper.

--- test_helpers.go
+++ test_helpers.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"database/sql"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -26,6 +29,52 @@ func TearDownDbTest(testDB *sql.DB) {
 	TearDownDB(testDB)
 }
 
+// SeededTestDB Creates a database object referring to a database seeded with
+// random test data. This is a way to approximate isolation in unit tests for
+// non-database functions, since this function is the only point of possible 
+// failure.
+// This function returns a map of "useful names" to ID's, in order to expedite
+// retrieval of relevant data. The syntax is ["(table name).(name/title/etc)."]
+// e.g. ids["user.nonadmin"] => 1
+//   or ids["note.note1"] => 2
+//   or ids["tag.tag1"] => 3
+func SeededTestDB() (db *sql.DB, ids map[string]int64) {
+	db = SetUpDbTest()
+	ids = map[string]int64{}
+
+	// Users
+	res, _ := db.Exec("INSERT INTO users (name, admin) VALUES (?, ?)", "nonadmin", false)
+	ids["user.nonadmin"], _ = res.LastInsertId()
+
+	res, _ = db.Exec("INSERT INTO users (name, admin) VALUES (?, ?)", "admin", true)
+	ids["user.admin"], _ = res.LastInsertId()
+
+	// Notes
+	res, _ = db.Exec("INSERT INTO notes (title, content, time, user_id) VALUES (?, ?, ?, ?)",
+		"note1", "content", "2017-01-01 12:00", ids["user.nonadmin"])
+	ids["note.note1"], _ = res.LastInsertId()
+
+	res, _ = db.Exec("INSERT INTO notes (title, content, time, user_id) VALUES (?, ?, ?, ?)",
+		"note2", "content", "2017-02-01 12:00", ids["user.nonadmin"])
+	ids["note.note2"], _ = res.LastInsertId()
+
+	// Tags
+	res, _ = db.Exec("INSERT INTO tags (title) VALUES (?)", "tag1")
+	ids["tag.tag1"], _ = res.LastInsertId()
+
+	res, _ = db.Exec("INSERT INTO tags (title) VALUES (?)", "tag2")
+	ids["tag.tag2"], _ = res.LastInsertId()
+
+	// Attach tags to notes. Note 1 will be tagged with tag 1, etc.
+	db.Exec("INSERT INTO note_tag (note_id, tag_id) VALUES (?, ?)",
+		ids["note.note1"], ids["tag.tag1"])
+
+	db.Exec("INSERT INTO note_tag (note_id, tag_id) VALUES (?, ?)",
+		ids["note.note2"], ids["tag.tag2"])
+
+	return 
+}
+
 func AssertEqual(expected interface{}, received interface{}, t *testing.T) {
 	if expected != received {
 		t.Errorf("Expected %v, received %v.", expected, received)
@@ -35,5 +84,17 @@ func AssertEqual(expected interface{}, received interface{}, t *testing.T) {
 func AssertUnequal(unexpected interface{}, received interface{}, t *testing.T) {
 	if unexpected == received {
 		t.Errorf("Did not expect %v to equal %v", received, unexpected)
+	}
+}
+
+func AssertSuccessCode(recorder *httptest.ResponseRecorder, t *testing.T) {
+	if status := recorder.Code; status != http.StatusOK {
+		t.Errorf("Wrong status code. Got %v want %v", status, http.StatusOK)
+	}
+}
+
+func AssertContains(haystack string, needle string, t *testing.T) {
+	if !strings.Contains(haystack, needle) {
+		t.Errorf("Could not find %s within %s.", needle, haystack)
 	}
 }
