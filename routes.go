1161934ca8bcaaf19eeb46c4bf1f4424691b83bb
Cade Gillem <cpgillem@gmail.com>
2017-11-04 19:04:59 -0400

    Add routes for existing handlers.

--- routes.go
+++ routes.go
@@ -21,7 +21,10 @@ func CreateRouter(context *Context) *mux.Router {
 	// Public Routes
 	router.HandleFunc("/", GetIndex(context))
 	router.HandleFunc("/login", PostLogin(context)).Methods("POST")
-	router.HandleFunc("/user/{id}", GetUser(context))
+	//router.HandleFunc("/logout", GetLogout(context)).Methods("GET")
+
+	router.HandleFunc("/user", PostUser(context)).Methods("POST")
+	router.HandleFunc("/user/{id}", GetUser(context)).Methods("GET")
 
 	// Authenticated Routes
 	router.Handle("/note/{id}", negroni.New(
