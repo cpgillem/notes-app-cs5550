ef54316fe3ac00056441f6dcffe2429512417cfb
Cade Gillem <cpgillem@gmail.com>
2017-11-04 19:28:12 -0400

    Rewrite GetUser.

--- user_handlers.go
+++ user_handlers.go
@@ -75,37 +75,6 @@ func GetIndex(context *Context) http.HandlerFunc {
 	}
 }
 
-// GetUser retrieves a user and their data by id.
-func GetUser(context *Context) http.HandlerFunc {
-	return func (w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-
-		if idVar, ok := vars["id"]; ok {
-			id, err := strconv.Atoi(idVar)
-			if err != nil {
-				http.Error(w, "URL does not contain a user ID.", http.StatusNotFound)
-				return
-			}
-
-			// Send the user struct with the password redacted.
-			// The id should be included along with the rest of the variables.
-			u, err := LoadUser(int64(id), context.DB)
-			if err != nil {
-				// Return a 404.
-				http.Error(w, "Could not find user.", http.StatusNotFound)
-				return
-			}
-
-			w.Header().Set("Content-Type", "application/json")
-			j, _ := json.Marshal(u)
-			w.Write(j)
-		} else {
-			// Return a 404.
-			http.Error(w, "Could not find user ID.", http.StatusNotFound)
-		}
-	}
-}
-
 func PostUser(context *Context) http.HandlerFunc {
 	return func (w http.ResponseWriter, r *http.Request) {
 		username := r.FormValue("username")
@@ -138,5 +107,35 @@ func PostUser(context *Context) http.HandlerFunc {
 			http.Error(w, "Could not store password.", http.StatusInternalServerError)
 			return
 		}
+	}
+}
+
+func GetUser(context *Context) http.HandlerFunc {
+	return func (w http.ResponseWriter, r *http.Request) {
+		// Retrieve the ID.
+		vars := mux.Vars(r)
+		idStr, ok := vars["id"]
+		if !ok {
+			http.Error(w, "No ID specified.", http.StatusNotFound)
+			return
+		}
+
+		// Convert the ID to an int.
+		uID, err := strconv.Atoi(idStr)
+		if err != nil {
+			http.Error(w, "Improper ID.", http.StatusNotFound)
+		}
+
+		// Retrieve a user model.
+		u, err := LoadUser(int64(uID), context.DB)
+		if err != nil {
+			http.Error(w, "User not found.", http.StatusNotFound)
+			return
+		}
+
+		// Return this user's data as a JSON response.
+		w.Header().Set("Content-Type", "application/json")
+		j, _ := json.Marshal(u)
+		w.Write(j)
 	}
 }
