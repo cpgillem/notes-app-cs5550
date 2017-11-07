a7cead54383d482b488bbfbc24d16aeb00ee92fe
Cade Gillem <cpgillem@gmail.com>
2017-11-06 19:52:28 -0500

    Remove unnecessary interface for models.

--- model.go
+++
@@ -1,9 +0,0 @@
-package csnotes
-
-// Model implements a function for saving to the database, and a function for
-// loading from it. They may use the helper methods Select, Sync, or Delete on
-// a resource, as needed.
-type Model interface {
-	Save() error
-	Load() error
-}
