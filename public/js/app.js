bb5c5644c426b0085e26cb85d966cdf3ae81acae
Cade Gillem <cpgillem@gmail.com>
2017-11-28 16:59:44 -0500

    Implement note editing and deleting.

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,23 @@
 // Core data functions
 
+function toList() {
+    $('#note-editor').hide();
+    $('#note-viewer').hide();
+    $('#note-list').show();
+}
+
+function toViewer() {
+    $('#note-editor').hide();
+    $('#note-list').hide();
+    $('#note-viewer').show();
+}
+
+function toEditor() {
+    $('#note-viewer').hide();
+    $('#note-list').hide();
+    $('#note-editor').show();
+}
+
 // Concatenates an authorization header for any requests to use.
 function getAuthHeader() {
   return "Bearer " + window.sessionStorage.accessToken;
@@ -32,10 +50,103 @@ function getNoteShowFunc(id) {
         $('#note-title').text(data.models[0].title);
         $('#note-date').text(data.models[0].date);
         $('#note-content').text(data.models[0].content.String);
-      } else if (data.errors) {
+      } else if (data.errors.length > 0) {
         // Display any errors.
         console.log(data.errors);
       }
+    });
+  };
+}
+
+function getNoteDeleteFunc(id) {
+  return function(e) {
+    // Confirm deletion.
+    if (confirm("Are you sure you would like to delete the note? This action cannot be undone.")) {
+      // Send delete request.
+      $.ajax({
+        url: getNoteUrl(id),
+        type: 'DELETE',
+        headers: {
+          'Authorization': getAuthHeader()
+        }
+      }).done(function(data) {
+        if (data.errors.length > 0) {
+          // Log errors.
+          console.log(errors);
+        } else {
+          // Update the table.
+          updateNoteList();
+        }
+      });
+    }
+  };
+}
+
+function getNoteUpdateFunc(id) {
+  return function(e) {
+    e.preventDefault();
+    // Hide help text under controls.
+    $('#note-title-help').hide();
+    $('#note-content-help').hide();
+
+    // Send the data in a PUT request.
+    $.ajax({
+      url: getNoteUrl(id),
+      type: 'PUT',
+      data: {
+        "title": $('#note-title-edit').val(),
+        "content": $('#note-content-edit').val()
+      },
+      headers: {
+        'Authorization': getAuthHeader()
+      }
+    }).done(function(data) {
+      // Log errors.
+      if (data.errors.length > 0) {
+        console.log(data.errors);
+      }
+
+      // Show help text under controls where the data is invalid.
+      if (data.fields["title"]) {
+        $('#note-title-help').text(data.fields["title"]);
+        $('#note-title-help').show();
+      } else if (data.fields["content"]) {
+        $('#note-content-help').text(data.fields["content"]);
+        $('#note-title-help').show();
+      } else {
+        updateNoteList();
+        toList();
+      }
+    });
+  };
+}
+
+function getNoteEditFunc(id) {
+  return function(e) {
+    // Show the correct view.
+    $('#note-list').hide();
+    $('#note-viewer').hide();
+    $('#note-editor').show();
+
+    // Query for the note's existing data.
+    $.ajax({
+      url: getNoteUrl(id),
+      type: 'GET',
+      headers: {
+        'Authorization': getAuthHeader()
+      }
+    }).done(function(data) {
+      // Fill out the form with existing data.
+      if (data.models[0]) {
+        var model = data.models[0];
+        $('#note-title-edit').val(model.title);
+        $('#note-content-edit').val(model.content.String);
+      } else if (data.errors.length > 0) {
+        console.log(data.errors);
+      }
+
+      // Set the correct function to store the data upon submit.
+      $('#note-update').on('click', getNoteUpdateFunc(id));
     });
   };
 }
@@ -62,10 +173,13 @@ function updateNoteList() {
         var row = $('<tr></tr>').appendTo('#note-table-body');
         row.append('<td>' + model.id + '</td>');
         row.append('<td>' + model.title + '</td>');
-        row.append('<td>' + model.date + '</td>');
         var actions = $('<td></td>').appendTo(row);
-        var editButton = $('<a href="#" class="btn">Show</a>').appendTo(actions);
-        editButton.on('click', getNoteShowFunc(model.id));
+        var showButton = $('<a href="#" class="btn">Show</a>').appendTo(actions);
+        var editButton = $('<a href="#" class="btn">Edit</a>').appendTo(actions);
+        var deleteButton = $('<a href="#" class="btn">Delete</a>').appendTo(actions);
+        showButton.on('click', getNoteShowFunc(model.id));
+        editButton.on('click', getNoteEditFunc(model.id));
+        deleteButton.on('click', getNoteDeleteFunc(model.id));
       }
     } else if (data.errors) {
       for (var i = 0; i < data.errors.length; i++) {
